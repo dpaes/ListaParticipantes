d253561b0c7c0758b04af0ddb364a03089e0ae5a
Daniel Paes <daniel_3XD@hotmail.com>
2023-07-28 21:59:00 -0300

    mod controller e criado home

--- src/main/java/com/MundoSenaiTDSN/ListaParticipantes/Controller/C_Pessoa.java
+++ src/main/java/com/MundoSenaiTDSN/ListaParticipantes/Controller/C_Pessoa.java
@@ -2,6 +2,8 @@ package com.MundoSenaiTDSN.ListaParticipantes.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class C_Pessoa {
@@ -11,4 +13,8 @@ public class C_Pessoa {
         return "Login/login";
     }
 
+    @PostMapping("/")
+    public String loginPessoa(@RequestParam("usuario") String usuario, @RequestParam("senha") String senha){
+        return "Home/home";
+    }
 }
